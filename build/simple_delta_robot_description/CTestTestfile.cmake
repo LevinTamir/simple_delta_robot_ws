70ae0be9045edf3fec0f8070ef49b12205e5ea11
Tamir Levin <tamirlvn@gmail.com>
2025-01-01 13:25:25 +0200

    update .gitignore file

--- build/simple_delta_robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tamir/ros2_workspaces/simple_delta_robot_ws/src/simple_delta_robot_description
-# Build directory: /home/tamir/ros2_workspaces/simple_delta_robot_ws/build/simple_delta_robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
