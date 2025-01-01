262cd95b43f8d35bf2b91eced90063280283a469
Tamir Levin <tamirlvn@gmail.com>
2025-01-01 13:13:45 +0200

    initial release

---
+++ build/simple_delta_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tamir/ros2_workspaces/simple_delta_robot_ws/src/simple_delta_robot_description
+# Build directory: /home/tamir/ros2_workspaces/simple_delta_robot_ws/build/simple_delta_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
